eee9f5d87799a451d60719c253839f8d8b9000a0
woshidaxian <huang15057081701@163.com>
2023-11-07 16:50:02 +0800

    vue.config

--- vue.config.js
+++ vue.config.js
@@ -15,6 +15,19 @@ module.exports = {
   // 生成的静态资源的文件名中是否包含了 hash
   filenameHashing: true, 
 
+  // MPA多页面模式
+  // pages: {
+  //   index: {
+  //     entry: 'src/main.js',
+  //     template: 'public/index.html',
+  //     filename: 'index.html', // 在dist/index.html 的输出，
+  //     title: '标题', // page title
+  //   },
+  //   otherPage: {
+
+  //   }
+  // },
+
   // 是否在开发环境下通过 eslint-loader 在每次保存时 lint 代码 boolean | 'warning' | 'default' | 'error'
   lintOnSave: 'default', 
 
@@ -25,5 +38,24 @@ module.exports = {
   // transpileDependencies: false,
 
   // 不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建
-  productionSourceMap: false
+  productionSourceMap: false,
+
+  // 设置生成的 HTML 中 <link rel="stylesheet"> 和 <script> 标签的 crossorigin 属性。仅控制构建时通过loader注入的
+  // crossorigin: ''
+
+  // 在生成的 HTML 中的 <link rel="stylesheet"> 和 <script> 标签上启用 Subresource Integrity (SRI)。
+  // integrity: ''
+
+  // 配置代理服务器
+  // devServer: ''
+  devServe: {
+    '/api': {
+      target: '<url>',
+      ws: true,
+      changeOrigin: true,
+    },
+    '/otherApi': {
+      target: '<otherApi>'
+    }
+  }
 }
